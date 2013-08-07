d0fb8dab91b9e96f909e73907d8ed4253853e9f9
Patrik Jira <patrikjira@gmail.com>
2013-08-07 19:02:13 +0200

    Deploy and related

---
+++ config.ru
@@ -0,0 +1,2 @@
+require './tracker'
+run Tracker
--- tracker.rb
+++ tracker.rb
@@ -3,28 +3,36 @@ require 'sinatra/config_file'
 require 'date'
 require 'letsfreckle'
 
-config_file 'config.yml'
 
-get '/' do
-  @from = if params[:from]
-            Date.parse(params[:from])
-          else
-            Date.new(Date.today.year, Date.today.month, 1)
-          end
 
-  @users = settings.users.map do |name, attrs|
-    if attrs['tracker'] == 'freckle'
-      LetsFreckle.configure do
-        account_host attrs['credentials']['host']
-        username attrs['credentials']['username']
-        token attrs['credentials']['token']
+class Tracker < Sinatra::Base
+
+  register Sinatra::ConfigFile
+  config_file 'config/config.yml'
+
+  get '/' do
+    @from = if params[:from]
+              Date.parse(params[:from])
+            else
+              Date.new(Date.today.year, Date.today.month, 1)
+            end
+
+    @users = settings.users.map do |name, attrs|
+      if attrs['tracker'] == 'freckle'
+        LetsFreckle.configure do
+          account_host attrs['credentials']['host']
+          username attrs['credentials']['username']
+          token attrs['credentials']['token']
+        end
       end
+
+      records = LetsFreckle::Entry.find(:from => @from.to_s)
+      {name: name, records: records}
     end
 
-    records = LetsFreckle::Entry.find(:from => @from.to_s)
-    {name: name, records: records}
+    erb :index
+
   end
 
-  erb :index
-
 end
+
