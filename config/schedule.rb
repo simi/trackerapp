ed141fad459e4b174023665ae898c9fcbfc44b75
Patrik Jira <patrikjira@gmail.com>
2013-09-01 18:36:49 +0200

    Even better whenever file

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,5 @@
+set :output, {:standard => 'cron.log'}
+set :environment_variable, "RACK_ENV"
 job_type :rake,    "cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
 
 every :day, :at => '0:02am' do
