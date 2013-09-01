da53d19275c05ab947f529e34e8d47ad13e0de24
Patrik Jira <patrikjira@gmail.com>
2013-09-01 18:34:36 +0200

    Better whenever file

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 job_type :rake,    "cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
 
-every :day, :at => '0:02am', :roles => [:app] do
+every :day, :at => '0:02am' do
   rake "import"
 end
 
-every :day, :at => '0am', :roles => [:app] do
+every :day, :at => '0am' do
   rake "export"
 end
