36c3bebb9f0bfb0cb8da8235f5ec442e3af6aaeb
pida <petr.snitily@gmail.com>
2013-12-09 08:21:49 +0100

    Added listing of entries for one month only. Also fixed not working mass assigment for project which made migration not work when creating desksnearme project. Also switched to d/m/y from m/d/y.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,15 +4,17 @@ class EntriesController < ApplicationController
 
   def index
     @from = if params[:from]
-      Date.strptime(params[:from], '%m/%d/%Y')
+      Date.strptime(params[:from], '%d/%m/%Y')
     else
       Date.new(Date.today.year, Date.today.month, 1)
     end
 
     @previous_month = (@from - 1.month).at_beginning_of_month
     @next_month = (@from + 1.month).at_beginning_of_month
+    puts @previous_month
+    puts @next_month
 
-    @entries = Entry.where(user_id: current_user.id).where('date >= ?', @from).order('date desc')
+    @entries = Entry.where(user_id: current_user.id).where('date >= ?', @from).where('date < ?', @next_month).order('date desc')
     @total = @entries.sum(:minutes)
 
   end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ActiveRecord::Base
   has_many :entries
   has_many :project_users
   has_many :users, :through => :project_users
+
+  attr_accessible :name
 end
