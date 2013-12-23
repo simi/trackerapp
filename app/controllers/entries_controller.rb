6604ad97e7b0ea090fb5148bd702bf43b5c6dc29
pida <petr.snitily@gmail.com>
2013-12-23 13:44:25 +0100

    Added imput validations.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,5 @@
 class EntriesController < ApplicationController
   before_filter :require_login
-  require "time_parser"
-
   autocomplete :project, :name, :full => true
 
   def index
@@ -16,6 +14,8 @@ class EntriesController < ApplicationController
 
     @entries = Entry.where(user_id: current_user.id).where('date >= ?', @from).where('date < ?', @next_month).order('date desc')
     @total = @entries.sum(:minutes)
+    
+    @entry ||= Entry.new
 
   end
 
@@ -24,24 +24,28 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @minutes = TimeParser::TimeParser.new(params[:entry][:time_spent]).minutes
-    @project_id = Project.find_by(name: params[:entry][:project_name]).id
-
+    @minutes = TimeParser::TimeParser.new(params[:entry][:time_spent]).minutes rescue nil
+    @project_id = Project.find_by(name: params[:entry][:project_name]).id rescue nil
+  
     @entry = Entry.new(entry_params)
-    @entry.save
+    if @entry.save
 
-    if not @entry.project.users.include?(current_user)
-      project_user = ProjectUser.new
-      project_user.user_id = current_user.id
-      project_user.project_id = @entry.project_id
-      project_user.save
-    end
+      if not @entry.project.users.include?(current_user)
+        project_user = ProjectUser.new
+        project_user.user_id = current_user.id
+        project_user.project_id = @entry.project_id
+        project_user.save
+      end
 
-    redirect_to entries_path
+      redirect_to entries_path, :notice => "Entry created."
+    else
+      index
+      render :index
+    end    
   end
 
   private
     def entry_params
-      params.require(:entry).permit(:description, :date).merge(minutes: @minutes, user_id: current_user.id, project_id: @project_id)
+      params.require(:entry).permit(:description, :date, :time_spent, :project_name).merge(minutes: @minutes, user_id: current_user.id, project_id: @project_id)
     end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,7 +2,11 @@ class Entry < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
 
-  attr_accessible :username, :description, :project_id, :minutes, :user_id, :date
+  attr_accessible :username, :description, :project_id, :minutes, :user_id, :date, :time_spent, :project_name
+  attr_accessor :time_spent, :project_name
+  
+  validates :date, presence: true, :allow_nil => false
+  validates :project_id, :minutes, presence: true, :allow_nil => false, allow_blank: false, :numericality => {:greater_than => 0}
 
   def formatted_minutes
     "#{self.minutes / 60}: #{self.minutes % 60}"
