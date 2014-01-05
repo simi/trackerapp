fcf78166a4ec816a63987dbb3c7e201eb027dd7a
pida <petr.snitily@gmail.com>
2014-01-05 16:05:17 +0100

    Moved login into before(:each).

--- spec/requests/entries_spec.rb
+++ spec/requests/entries_spec.rb
@@ -1,72 +1,69 @@
 require 'spec_helper'
 
 describe "Entries" do
-  describe "GET /entries" do
-    it "lists existing entries" do
-      user = FactoryGirl.create(:user)
-      project = FactoryGirl.create(:project)
-      entry = FactoryGirl.create(:entry)
-      entry_previous = FactoryGirl.create(:entry, date: (Date.today - 1.month).strftime("%d/%m/%Y"))
-      entry_old = FactoryGirl.create(:entry, date: (Date.today - 3.month).strftime("%d/%m/%Y"))
-      entry_next = FactoryGirl.create(:entry, date: (Date.today + 1.month).strftime("%d/%m/%Y"))
-      FactoryGirl.create(:ProjectUser)
+  before(:each) do
+    user = FactoryGirl.create(:user)
+    project = FactoryGirl.create(:project)
+    FactoryGirl.create(:ProjectUser)
 
-      visit "/"
-      fill_in 'username', with: user.username
-      fill_in 'password', with: "secret"
-      click_button 'Log in'
-      page.should have_content("test description 1")
+    visit "/"
+    fill_in 'username', with: user.username
+    fill_in 'password', with: "secret"
+    click_button 'Log in'
+  end
 
-      first(:link, (Date.today - 1.month).strftime("%B %Y")).click
-      page.should have_content("test description 2")
+  it "lists existing entries" do
+    entry = FactoryGirl.create(:entry)
+    entry_previous = FactoryGirl.create(:entry, date: (Date.today - 1.month).strftime("%d/%m/%Y"))
+    entry_old = FactoryGirl.create(:entry, date: (Date.today - 3.month).strftime("%d/%m/%Y"))
+    entry_next = FactoryGirl.create(:entry, date: (Date.today + 1.month).strftime("%d/%m/%Y"))
 
-      first(:link, (Date.today - 2.month).strftime("%B %Y")).click
-      first(:link, (Date.today - 3.month).strftime("%B %Y")).click
-      page.should have_content("test description 3")
+    visit "/"
+    page.should have_content("test description 1")
 
-      first(:link, "Tracker").click
-      page.should have_content("test description 1")
+    first(:link, (Date.today - 1.month).strftime("%B %Y")).click
+    page.should have_content("test description 2")
 
-      first(:link, (Date.today + 1.month).strftime("%B %Y")).click
-      page.should have_content("test description 4")
+    first(:link, (Date.today - 2.month).strftime("%B %Y")).click
+    first(:link, (Date.today - 3.month).strftime("%B %Y")).click
+    page.should have_content("test description 3")
 
-    end
+    first(:link, "Tracker").click
+    page.should have_content("test description 1")
 
-    it "creates entry" do
-      user = FactoryGirl.create(:user)
-      project = FactoryGirl.create(:project)
-      visit "/"
-      fill_in 'username', with: user.username
-      fill_in 'password', with: "secret"
-      click_button 'Log in'
-      page.should have_content("Total:  0:0")
+    first(:link, (Date.today + 1.month).strftime("%B %Y")).click
+    page.should have_content("test description 4")
 
-      fill_in 'entry_time_spent', with: "1.5 h"
-      fill_in 'entry_project_name', with: "test project"
-      fill_in 'entry_description', with: "test description 5"
-      click_button 'Add'
-      page.should have_content("test description 5")
+  end
 
-      fill_in 'entry_time_spent', with: "130"
-      fill_in 'entry_project_name', with: "test project"
-      fill_in 'entry_description', with: "test description 6"
-      fill_in 'entry_date', with: (Date.today - 1.month).strftime("%d/%m/%Y")
-      click_button 'Add'
-      first(:link, (Date.today - 1.month).strftime("%B %Y")).click
-      page.should have_content("test description 6")
+  it "creates entry" do
+    page.should have_content("Total:  0:0")
 
-      fill_in 'entry_time_spent', with: "1:15"
-      fill_in 'entry_project_name', with: "test project"
-      fill_in 'entry_description', with: "test description 7"
-      fill_in 'entry_date', with: (Date.today + 1.month).strftime("%d/%m/%Y")
-      click_button 'Add'
-      first(:link, (Date.today - 1.month).strftime("%B %Y")).click
-      first(:link, "Tracker").click
-      page.should have_content("test description 5")
+    fill_in 'entry_time_spent', with: "1.5 h"
+    fill_in 'entry_project_name', with: "test project"
+    fill_in 'entry_description', with: "test description 5"
+    click_button 'Add'
+    page.should have_content("test description 5")
 
-      first(:link, (Date.today + 1.month).strftime("%B %Y")).click
-      page.should have_content("test description 7")
+    fill_in 'entry_time_spent', with: "130"
+    fill_in 'entry_project_name', with: "test project"
+    fill_in 'entry_description', with: "test description 6"
+    fill_in 'entry_date', with: (Date.today - 1.month).strftime("%d/%m/%Y")
+    click_button 'Add'
+    first(:link, (Date.today - 1.month).strftime("%B %Y")).click
+    page.should have_content("test description 6")
 
-    end
+    fill_in 'entry_time_spent', with: "1:15"
+    fill_in 'entry_project_name', with: "test project"
+    fill_in 'entry_description', with: "test description 7"
+    fill_in 'entry_date', with: (Date.today + 1.month).strftime("%d/%m/%Y")
+    click_button 'Add'
+    first(:link, (Date.today - 1.month).strftime("%B %Y")).click
+    first(:link, "Tracker").click
+    page.should have_content("test description 5")
+
+    first(:link, (Date.today + 1.month).strftime("%B %Y")).click
+    page.should have_content("test description 7")
+
   end
 end
