7428169e046aed36651bb39a5b24498434733e79
Patrik Jira <patrikjira@gmail.com>
2014-02-25 12:49:26 +0100

    Feat: font-face

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ Bundler.require(:default, Rails.env)
 module Trackerapp
   class Application < Rails::Application
     config.autoload_paths += %W(#{Rails.root}/lib/)
+    config.assets.paths << %(#{Rails.root}/app/assets/fonts)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
