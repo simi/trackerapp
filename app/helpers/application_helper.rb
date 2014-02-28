a1a70e1264ba41f413a72b2bb94e3b2fb55ad548
Patrik Jira <patrikjira@gmail.com>
2014-02-28 11:44:15 +0100

    Feat: add top navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,25 @@ module ApplicationHelper
     result
   end
 
+  def alert_kind(flash)
+      case flash
+      when :notice
+        'success'
+      when :alert
+        'warning'
+      end
+  end
+
+  def nav_item_class(item_name)
+    active = case item_name.to_s
+             when 'root'
+               params[:controller] == 'entries'
+             when 'admin'
+              ['admin/projects', 'admin/users'].include?(params[:controller])
+             end
+
+    classes = ['nav-item']
+    classes << 'active' if active
+    classes.join(" ")
+  end
 end
