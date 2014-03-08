84922f7fb22c6e2b996ffe5b326083343d3df34c
pida <petr.snitily@gmail.com>
2014-03-08 20:49:30 +0100

    Feat: continue with translation, entries done.

--- app/forms/entry_form.rb
+++ app/forms/entry_form.rb
@@ -24,7 +24,7 @@ class EntryForm < Form
   def date_in_words
     # if date was set to today, or wasnt set at all
     if (@date.present? && @date == Date.current) || @date.blank?
-      I18n.t('today_change').html_safe
+      I18n.t('entries.today_change').html_safe
     else
       I18n.l(@date, :format => "d% %B, %Y")
     end
